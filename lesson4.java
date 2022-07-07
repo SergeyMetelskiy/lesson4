652912a30e97d626fb8f04fae075638cad4049e0
SergeyMetelskiy <108751783+SergeyMetelskiy@users.noreply.github.com>
2022-07-07 21:37:37 +0300

    Add files via upload

---
+++ lesson4.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+public class lesson4 {
+
+    public static void main(String[] args){
+       /*
+        //if1. дано целое число. Если оно положительное, то прибавить к
+        // нему 1, а в противном случае не изменять его. Вывести полученное число.
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        if (a>0){
+            a++;
+        }
+        System.out.println(a);
+
+        */
+      /* // If2. Дано целое число. Если оно является положительным, то прибавить к
+       // нему 1; в противном случае вычесть из него 2. Вывести полученное число.
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        if (a > 0){
+            a += 1;
+        }
+        else {
+            a = a - 2;
+
+        }
+        */
+        /*//If3. Дано целое число. Если оно является положительным, то прибавить к
+        //нему 1; если отрицательным, то вычесть из него 2; если нулевым, то
+        //заменить его на 10. Вывести полученное число.
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        int a = in.nextInt();
+        if (a>0){
+            a+=1;
+        }
+        else if (a<0){
+            a-=2;
+            }
+            else {
+                a=10;
+            }
+        System.out.println(a);
+         */
+        /*//If6◦
+        //If7. Даны два числа. Вывести порядковый номер меньшего из них.
+        Scanner in = new Scanner(System.in);
+        int num1 = in.nextInt();
+        int num2 = in.nextInt();
+        if (num1<num2){
+            System.out.println("1");
+        }
+        if (num1>num2){
+            System.out.println("2");
+        }
+        */
+       /* Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        if (a!=b) {
+            if (a > b) {
+                b = a;
+            } else {
+                a = b;
+            }
+        }
+            else{
+                a=0;
+                b=0;
+            }
+        */
+
+       /* //If15. Даны три числа. Найти сумму двух наибольших из них.
+        System.out.println("Введите число: ");
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int res=0;
+        if (a<b&&a<c)res=b+c;
+        else if (a>b&&b>c)res=a+b;
+        else if (a>b&&b<c)res=a+c;
+
+        */
+
+
+
+
+
+    }
+}
